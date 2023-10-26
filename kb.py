a46289d5c4d761ad44a5d39651e5f21b754ab78a
simon <englishtea21@mail.ru>
2023-10-26 12:59:58 +0300

    altering buttons

--- kb.py
+++ kb.py
@@ -11,10 +11,16 @@ start keyboard with options
 """
 START_INSTRUCTIONS = ReplyKeyboardMarkup(
     keyboard=[
-        [KeyboardButton(text=text_templates["OPTIONS"]["PHOTO_REQUIREMENTS_QUESTION"])],
-        [KeyboardButton(text=text_templates["OPTIONS"]["IMPORTANT_QUESTION"])],
-        [KeyboardButton(text=text_templates["OPTIONS"]["MUSHROOMS_BASE_QUESTION"])],
-        [KeyboardButton(text=text_templates["OPTIONS"]["ABOUT_PROJECT_QUESTION"])],
+        [
+            KeyboardButton(
+                text=text_templates["OPTIONS"]["PHOTO_REQUIREMENTS_QUESTION"]
+            ),
+            KeyboardButton(text=text_templates["OPTIONS"]["IMPORTANT_QUESTION"]),
+        ],
+        [
+            KeyboardButton(text=text_templates["OPTIONS"]["MUSHROOMS_BASE_QUESTION"]),
+            KeyboardButton(text=text_templates["OPTIONS"]["ABOUT_PROJECT_QUESTION"]),
+        ],
     ],
     input_field_placeholder=text_templates["SELECT_OPTIONS_BELOW"],
 )
